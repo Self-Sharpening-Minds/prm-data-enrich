555fcf6fdcc648fd9d647752bc9219ce691e90ef
Petros607 <markaryan.petr@mail.ru>
2025-11-10 19:28:13 +0400

    Новый формат логов

--- logger.py
+++ logger.py
@@ -1,12 +1,16 @@
 import logging
 import sys
+from datetime import datetime
+from pathlib import Path
 
 
-def setup_logging(level=logging.INFO):
-    """Настройка логирования для всего проекта"""
+def setup_logging(level=logging.INFO, log_dir: str = "logs"):
+    """Настройка логирования для всего проекта
+    [PRM-0] [2025-10-17 18:12:35] [file:34:func] - level - message
+    """
 
     formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        '[PRM-ENRICH] [%(asctime)s] [%(name)s:%(lineno)s:%(funcName)s] - %(levelname)s - %(message)s'
     )
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(formatter)
